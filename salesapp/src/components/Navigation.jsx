1e702ef989d473498315b5662edcce4e02c9c076
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-11-24 14:51:50 +0530

    Navigation Bar

---
+++ salesapp/src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import "./Navigation.css";
+export const Navigation = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li><a href="www.pickupbiz.com" target="_blank">
+          PickupBiz
+        </a></li>
+        <li><a href="www.amazon.com" target="_blank">
+          Amazon
+        </a></li>
+        <li><a href="www.google.com" target="_blank">
+          Google
+        </a></li>
+        <li><a href="www.gmail.com" target="_blank">
+          Gmail
+        </a></li>
+        <li><a href="www.naukri.com" target="_blank">
+          Naukri
+        </a></li>
+      </ul>
+    </div>
+  );
+};
