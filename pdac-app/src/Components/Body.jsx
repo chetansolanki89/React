dc1be402753805950a349ed056e498415798ccab
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-11-24 16:41:12 +0530

    Components

---
+++ pdac-app/src/Components/Body.jsx
@@ -0,0 +1,10 @@
+const Body=()=>{
+    return(
+        <div className="body">
+            <h1>This is Main Body</h1>
+            <p>This is a paragraph.</p>
+        </div>
+    )
+}
+
+export default Body;
---
+++ pdac-app/src/Components/Navigation.jsx
@@ -0,0 +1,25 @@
+export const Navigation = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">HTML</a>
+        </li>
+        <li>
+          <a href="#">CSS</a>
+        </li>
+        <li>
+          <a href="#">JavaScript</a>
+        </li>
+        <li>
+          <a href="#">React</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// export default Navigation;
