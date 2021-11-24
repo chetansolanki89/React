c95ffe9d1b91fe334ea8f43ae24c1d29ed4ddad4
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-11-24 16:40:57 +0530

    Practice App

---
+++ pdac-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Navigation } from "./Components/Navigation";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
