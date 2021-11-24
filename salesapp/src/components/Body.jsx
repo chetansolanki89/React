610fe725cb5b3eccf530641eb5036560ad6cb0c7
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-11-24 14:52:17 +0530

    Body section

---
+++ salesapp/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import "./Body.css"
+export const Body = () => {
+  return (
+    <div className="body">
+      <div className="left"></div>
+      <div className="center"></div>
+      <div className="right">
+        <div className="up"></div>
+        <div className="down"></div>
+      </div>
+    </div>
+  );
+};
