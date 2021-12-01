7cccbd6eb135f798537a481d1e36fa148aadd811
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-12-01 14:39:38 +0530

    Class Component

---
+++ demoapp/src/Components/classcomponent/ClassComp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ClassComp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Class Component</h1>
+      </div>
+    );
+  }
+}
+export default ClassComp;
---
+++ demoapp/src/Components/classcomponent/ClassComp1.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ClassComp1 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Thi is Class Component 1</h2>
+      </div>
+    );
+  }
+}
+export default ClassComp1;
