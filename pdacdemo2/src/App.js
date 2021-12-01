8372bcd0f988919abf3fc097b984a798ea739157
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-12-01 14:43:46 +0530

    Updates

---
+++ pdacdemo2/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BikesList } from "./Components/BikesList";
+import { ProductList } from "./Components/ProductList";
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProductList />
+      {/* <BikesList/> */}
+    </div>
+  );
+};
+
+export default App;
