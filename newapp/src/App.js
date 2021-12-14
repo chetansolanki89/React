488e7d38f99519930238be680f73ffb3601dafd0
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-12-14 10:56:56 +0530

    Updates

---
+++ newapp/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Routing from "./Components/Routing";
+
+function App() {
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
