139f0c89b87b813db38e637e33e7563f2892e961
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-12-22 07:32:52 +0530

    Deleted

--- practiceapp/src/Redux/Store/configStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { productsReducer } from "../Reducer/productsReducer";
-export const configStore = () => {
-  const myStore = createStore(
-    combineReducers({ productsReducer }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  return myStore;
-};
