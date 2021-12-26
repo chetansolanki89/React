3d376871a5ef9ed26a7e9e0e3499851d1c2e52a9
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-12-26 18:18:40 +0530

    Initial Commit

---
+++ interview/src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import InputTable from "./Components/InputTable/InputTable";
+// import Resume from "./Components/Resume/Resume";
+import Parent from "./Components/Sohel/Parent";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <InputTable /> */}
+      {/* <Resume/> */}
+      <Parent/>
+    </div>
+  );
+}
+
+export default App;
