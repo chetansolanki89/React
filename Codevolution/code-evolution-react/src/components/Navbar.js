16711344df995e91107837731fcd13065eae99c5
Chetan R. Solanki <chetansolanki.89@gmail.com>
2022-02-24 17:04:16 +0530

    Routing completed with Authentication and Protected Routing

--- Codevolution/code-evolution-react/src/components/Navbar.js
+++ Codevolution/code-evolution-react/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 const Navbar = () => {
   const navLinkStyles = ({ isActive }) => {
@@ -9,14 +10,30 @@ const Navbar = () => {
     };
   };
 
+  const auth = useAuth();
+
   return (
-    <nav>
+    <nav className="primary-nav">
       <NavLink style={navLinkStyles} to="/">
         Home
       </NavLink>
       <NavLink style={navLinkStyles} to="/about">
         About
       </NavLink>
+      <NavLink style={navLinkStyles} to="/products">
+        Products
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/users">
+        Users
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/profile">
+        Profile
+      </NavLink>
+      {!auth.user && (
+        <NavLink style={navLinkStyles} to="/login">
+          Login
+        </NavLink>
+      )}
     </nav>
   );
 };
